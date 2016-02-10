b5c58e058c8638655b7bc41fae941bdb04ef72b0
Nathan Arthur <narthur@preemptive.com>
2016-02-09 21:36:13 -0500

    Improve run_server to allow minimal command-line arguments

--- run_server.sh
+++ run_server.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
+DECK="$1"
+
+if [ -z "$DECK" ]; then
 	echo "You must specify a subdir that has the index.html you want to present" >&2
 	exit 1
 fi
 
-if [ ! -d "$1" ]; then
-	echo "$1 is not a directory" >&2
-	exit 2
+if [ ! -d "$DECK" ]; then
+	if [ -d "slides/$DECK" ]; then
+		DECK="slides/$DECK"
+	else
+		echo "$DECK is not a directory" >&2
+		exit 2
+	fi
 fi
 
-grunt serve --webdir=$1
+grunt serve --webdir=$DECK
